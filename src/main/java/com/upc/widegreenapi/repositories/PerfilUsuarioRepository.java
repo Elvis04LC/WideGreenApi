fc7cb695c132f2951adc5054ddc601af6c405116
USUARIO <USUARIO@DESKTOP-2QTAKFF>
2025-07-02 02:48:08 -0500

    Se agrego en perfilUsuario y PublicacionRepository query de findbyusuarioid, para listar las publicaciones de un determinado usuario autenticado

--- src/main/java/com/upc/widegreenapi/repositories/PerfilUsuarioRepository.java
+++ src/main/java/com/upc/widegreenapi/repositories/PerfilUsuarioRepository.java
@@ -4,6 +4,11 @@ import com.upc.widegreenapi.entities.PerfilUsuario;
 import com.upc.widegreenapi.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PerfilUsuarioRepository extends JpaRepository<PerfilUsuario, Long> {
     boolean existsByUsuario(Usuario usuario);
+
+    Optional<PerfilUsuario> findByUsuario(Usuario usuario);
+
 }
--- src/main/java/com/upc/widegreenapi/repositories/PublicacionRepository.java
+++ src/main/java/com/upc/widegreenapi/repositories/PublicacionRepository.java
@@ -3,9 +3,12 @@ package com.upc.widegreenapi.repositories;
 import com.upc.widegreenapi.entities.Publicacion;
 import com.upc.widegreenapi.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PublicacionRepository extends JpaRepository<Publicacion, Long> {
-    List<Publicacion> findByUsuario(Usuario usuario);
+    @Query("SELECT p FROM Publicacion p WHERE p.usuario.idUsuario = :idUsuario")
+    List<Publicacion> findByUsuarioId(@Param("idUsuario") Long idUsuario);
 }
